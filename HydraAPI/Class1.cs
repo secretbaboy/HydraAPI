22bc6161e7b34dfcb4ff1bdb9d2f4c9a4f9cb17e
secretbaboy <kelvintang94@gmail.com>
2015-01-23 15:26:39 +0800

    Added first function

---
+++ HydraAPI/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace HydraAPI
+{
+    public class Initialize
+    {
+        public void start_hadoop(string path)
+        {
+            Process.Start("cmd", @"/c cd " + path + " && start-dfs.cmd");  
+        }
+        
+    }
+}
